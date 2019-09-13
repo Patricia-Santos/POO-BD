7a0e5f729a701b0b62c6becf13ee808014589274
Aluno <Aluno@LAB01-PC12>
2019-09-13 09:24:53 -0300

    Hello

--- POO-BD/src/DAO.java
+++ POO-BD/src/DAO.java
@@ -8,35 +8,29 @@ public class DAO<E extends Identificavel> {
 	
 
 	private EntityManagerFactory factory = Persistence.createEntityManagerFactory("salao");
+	EntityManager em = factory.createEntityManager();
 	
 	public void save(E e) {
-		EntityManager em = factory.createEntityManager();
+		
 		em.getTransaction().begin();
 		em.persist(e);
 		em.getTransaction().commit();
-		em.close();
 	}
 	
 	public void update(E e) {
-		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
 		em.merge(e);
 		em.getTransaction().commit();
-		em.close();
 	}
 	
 	public void remove(E e) {
-		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
 		em.remove(e);
 		em.getTransaction().commit();
-		em.close();
 	}
 	
 	public E find(Class<E> classe,Long id) {
-		EntityManager em = factory.createEntityManager();
 		E e = em.find(classe, id);
-		em.close();
 		return e;
 	}
 	
